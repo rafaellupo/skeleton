87809b8b16d9616b3c819933b431d37cb4321b8e
Rafael <rafael@MBP-de-Rafael.home>
2016-06-24 06:05:29 -0300

    migrations

--- config/database.php
+++ config/database.php
@@ -54,16 +54,16 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'laravel.cwx244igmbxw.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'rafaellupo'),
+            'password' => env('DB_PASSWORD', 'rafaellupo'),
+            'charset' => 'latin1',
+            'collation' => 'latin1_swedish_ci',
             'prefix' => '',
             'strict' => false,
-            'engine' => null,
+            'engine' => 'InnoDB',
         ],
 
         'pgsql' => [
