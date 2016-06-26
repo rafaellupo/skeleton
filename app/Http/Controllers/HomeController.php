fe4a14825e3edc7e5903169bdd86e7341c40be48
rafaellupo <rafael@MBP-de-Rafael.home>
2016-06-26 02:13:47 -0300

    newway

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
     public function __construct()
     {
         //$this->middleware('auth');
-        $this->middleware('cors');
+        //$this->middleware('cors');
         //git
     }
 
