e4eea4a26044d3e995758d73c4eb152ac07267a5
rafaellupo <rafael@MBP-de-Rafael.home>
2016-06-26 03:38:20 -0300

    mudei o header e csrfkaa

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,8 +10,9 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-header('Access-Control-Allow-Origin:  *');
+$http_origin = $_SERVER['HTTP_ORIGIN'];
+header('Access-Control-Allow-Origin:' . $http_origin);
+header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
